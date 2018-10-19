81bc6bd2a35aa89c4d4fb046d8677b860921e5ee
sunxiaobo <sunxiaobo@jd.com>
2018-10-19 17:56:01 +0800

    implement serializer

--- angie-rpc/src/main/java/com/chopin/sunny/SerializerEngine.java
+++ angie-rpc/src/main/java/com/chopin/sunny/SerializerEngine.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SerializerEngine {
-
     private static Map<SerializerType,ISerializer> serializeMap = new ConcurrentHashMap<>();
     static{
         serializeMap.put(SerializerType.DefaultJavaSerializer, new DefaultJavaSerializer());
@@ -18,6 +17,46 @@ public class SerializerEngine {
         serializeMap.put(SerializerType.ProtoBufSerializer, new ProtoBufSerializer());
         serializeMap.put(SerializerType.ProtoStuffSerializer, new ProtoStuffSerializer());
         serializeMap.put(SerializerType.ThriftSerializer, new ThriftSerializer());
+    }
+
+    public <T> byte[] serialize(T obj,String serializeType){
+        SerializerType st =  SerializerType.queryByType(serializeType);
+        if(st==null){
+            throw new RuntimeException("unsupport serializeType");
+        }
+        ISerializer serializer = serializeMap.get(st);
+        if(serializer == null){
+            throw new RuntimeException("unsupport serializer");
+        }
+        try{
+            return serializer.serialize(obj);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * 反序列化
+     * @param data
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public <T> T deserialize(byte[] data, Class<T> clazz,String serializeType){
+        SerializerType st =  SerializerType.queryByType(serializeType);
+        if(st==null){
+            throw new RuntimeException("unsupport serializeType");
+        }
+        ISerializer serializer = serializeMap.get(serializeType);
+        if(serializer == null){
+            throw new RuntimeException("unsupport serializer");
+        }
+        try{
+            return serializer.deserialize(data,clazz);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
 }

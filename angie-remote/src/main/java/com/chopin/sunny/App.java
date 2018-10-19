745741c1acf8e44cc575bbe3c1c9b636a32b9604
sunxiaobo <sunxiaobo@jd.com>
2018-10-19 17:02:27 +0800

    init

---
+++ angie-remote/src/main/java/com/chopin/sunny/App.java
@@ -0,0 +1,13 @@
+package com.chopin.sunny;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
